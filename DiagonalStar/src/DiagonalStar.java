7180161c25ac2df3af16f8bfe558937bb0c46eb2
Maria Gomez <maria_gg86@yahoo.es>
2021-08-21 13:22:12 +0200

    Reorganizado código para separar el pintado de los lados del cuadrado de las diagonales principales y secundarias

--- DiagonalStar/src/DiagonalStar.java
+++ DiagonalStar/src/DiagonalStar.java
@@ -12,14 +12,15 @@ public class DiagonalStar {
 
         for (int row = 0; row < number; row++) {
             for (int col = 0; col < number; col++) {
-                if (row == 0 || row == col || col == 0 || row == (number - 1) ||
-                        col == (number - 1) || row + col == (number - 1)) {
+                if (row == 0 || col == 0 || row == (number - 1) || col == (number - 1) ||
+                        row == col || row + col == (number - 1)) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
-                System.out.println(); // creates a new row
             }
+
+            System.out.println(); // creates a new row
         }
 
     }
