81f156d2a44f8341b7e577ed3d65c1fa76dcb783
Maria Gomez <maria_gg86@yahoo.es>
2021-09-21 17:23:27 +0200

    finalizado ejercicio explicativo ArrayList

--- ReverseArray/src/ReverseArray.java
+++ ReverseArray/src/ReverseArray.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class ReverseArray {
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+        System.out.println("Array = " + Arrays.toString(array));
+        reverse(array);
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+
+    static void reverse(int[] array) {
+
+        if (array != null) {
+            int n = array.length;
+            int half = n / 2;
+            for (int i = 0; i < half; i++) {
+                int left = i;
+                int right = n - 1 - i;
+                // We swap the elements
+                swap(array, left, right);
+            }
+        }
+    }
+
+    private static void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
 }
--- ReverseArray/src/ReverseArrayTest.java
+++ ReverseArray/src/ReverseArrayTest.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ReverseArrayTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseArrayTest {
+
+    @Test
+    public void givenAnEmptyArray_whenReverse_thenEmptyArray() {
+        int[] array = {};
+        ReverseArray.reverse(array);
+        Assert.assertArrayEquals(new int[0], array);
+    }
+
+    @Test
+    public void givenANullArray_whenReverse_thenNullArray() {
+        int[] array = null;
+        ReverseArray.reverse(array);
+        Assert.assertNull(array);
+    }
+
+    @Test
+    public void givenOneElementArray_whenReverse_thenOneElementArray() {
+        int[] array = {1};
+        ReverseArray.reverse(array);
+        Assert.assertArrayEquals(new int[]{1}, array);
+    }
+
+    @Test
+    public void givenAnArrayWithSomeElements_whenReverse_thenArrayWithSomeElementsReversed() {
+        int[] array = {1, 2, 3, 4};
+        ReverseArray.reverse(array);
+        Assert.assertArrayEquals(new int[]{4, 3, 2, 1}, array);
+    }
 }
