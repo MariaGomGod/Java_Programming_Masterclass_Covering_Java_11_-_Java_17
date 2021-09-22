c2741e689581d498e0ef8c802934c736afe8b62a
Maria Gomez <maria_gg86@yahoo.es>
2021-09-22 16:55:01 +0200

    implementados tests del ejercicio MobilePhone

---
+++ MobilePhone/src/MobilePhoneTest.java
@@ -0,0 +1,97 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MobilePhoneTest {
+
+    @Test
+    public void givenAnEmptyContactList_whenAddNewContact_itIsAdded() {
+        MobilePhone p = new MobilePhone(null);
+
+        boolean result = p.addNewContact(new Contact("María", null));
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenAContact_whenAddNewContact_itIsAdded() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("María", null));
+
+        boolean result = p.addNewContact(new Contact("Pedro", null));
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenAContact_whenAddAnExistingContact_itIsNotAdded() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("María", null));
+
+        boolean result = p.addNewContact(new Contact("María", null));
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenAContact_whenAddAnExistingContactWhichIsNotTheFirst_itIsNotAdded() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("Pedro", null));
+        p.addNewContact(new Contact("María", null));
+
+        boolean result = p.addNewContact(new Contact("María", null));
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenAnEmptyContactList_whenRemoveContact_itIsNotRemoved() {
+        MobilePhone p = new MobilePhone(null);
+
+        boolean result = p.removeContact(new Contact("María", null));
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenAContact_whenRemoveContact_itIsNotRemoved() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("María", null));
+
+        boolean result = p.removeContact(new Contact("Pedro", null));
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenAContact_whenRemoveAnExistingContact_itIsRemoved() {
+        MobilePhone p = new MobilePhone(null);
+        Contact contact = new Contact("María", null);
+        p.addNewContact(contact);
+
+        boolean result = p.removeContact(contact);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenAContact_whenRemoveAnExistingContactWhichIsNotTheFirst_itIsRemoved() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("Pedro", null));
+        Contact contact = new Contact("María", null);
+        p.addNewContact(contact);
+
+        boolean result = p.removeContact(contact);
+
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenAContact_whenRemoveAnExistingContactWithAnotherObject_itIsNotRemoved() {
+        MobilePhone p = new MobilePhone(null);
+        p.addNewContact(new Contact("María", null));
+
+        boolean result = p.removeContact(new Contact("María", null));
+
+        Assert.assertFalse(result);
+    }
+}
