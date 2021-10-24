f10d4e7766923ea2d27dc155c243b0d6856af147
Maria Gomez <maria_gg86@yahoo.es>
2021-10-24 14:53:18 +0200

    finalizado ejemplo alcance: Scope

--- Scope/src/com/timbuchalka/ScopeCheck.java
+++ Scope/src/com/timbuchalka/ScopeCheck.java
@@ -2,35 +2,40 @@ package com.timbuchalka;
 
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int var1 = 1;
 
     public ScopeCheck() {
-        System.out.println("Scopecheck created, publicVar = " + publicVar + ": pirvateVar = " + privateVar);
+        System.out.println("Scopecheck created, publicVar = " + publicVar + ": var1 = " + var1);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVar1() {
+        return var1;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int var2 = 2;
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + " times two is " + i * privateVar);
-            //System.out.println(i + " times two is " + i * this.privateVar); // con this.privateVar le queremos decir a Java que utilice la variable privateVar definida al principio con el valor 1
+            System.out.println(i + " times two is " + i * var2);
+            //System.out.println(i + " times two is " + i * this.var2); // con this.var2 le queremos decir a Java que utilice la variable var2 definida al principio con el valor 1
         }
     }
 
+    public void useInner() {
+        InnerClass innerclass = new InnerClass();
+        System.out.println("var3 from outer class: " + innerclass.var3);
+    }
+
     public class InnerClass {
-        public int privateVar = 3;
+        private int var3 = 3;
 
         public InnerClass() {
-            System.out.println("InnerClass created, privateVar is " + privateVar);
+            System.out.println("InnerClass created, var1 is " + var1 + " and var3 is " + var3);
         }
 
         public void timesTwo() {
-            int privateVar = 2;
+            System.out.println("var1 is still available here " + var1);
             for (int i = 0; i < 10; i++) {
-                System.out.println(i + " times two is " + i * privateVar);
+                System.out.println(i + " times two is " + i * var3);
             }
         }
     }
